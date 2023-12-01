ef82f71351a89fd183bae6c7e834a0f52a26d332
git <git@github.com>
2023-12-01 21:39:07 +0200

    Added a small game and refactored some files.

--- accountload.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('name').innerHTML = users[loggedInIndex].name;
-document.getElementById('email').innerHTML = users[loggedInIndex].email;
-document.getElementById('username').innerHTML = users[loggedInIndex].username;
--- script.js
+++ script.js
@@ -1,3 +1,141 @@
+class Item {
+    constructor(imagePath, productName, price) {
+        this.imagePath = imagePath;
+        this.productName = productName;
+        this.price = price;
+    }
+}
+
+class User {
+    constructor(name, username, password, email, gender, dateOfBirth) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.cart = [];
+    }
+
+}
+
+class Trivia {
+    constructor(question, answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
+
+const sampleItem = new Item('images/blacktshirt.jpg',
+    'Black T-Shirt',
+    17.99);
+
+const sampleUser = new User('Kareem Ramzi',
+    'kareemramzi',
+    '123',
+    'kareem@ramzi.com',
+    'Male',
+    '1990-01-01');
+
+var users = JSON.parse(localStorage.getItem('users')) || [];
+var loggedInIndex = JSON.parse(localStorage.getItem('loggedInIndex'));
+var questions = [
+    new Trivia("Which vegetable is believed to make you see better?", "Carrot"),
+    new Trivia("What is the tallest animal?", "Giraffe"),
+    new Trivia("What is the yellow part of an egg called?", "Yolk"),
+    new Trivia("Where are the Great Pyramids of Giza located?", "Egypt"),
+    new Trivia("What does Na stand for on the periodic table?", "Sodium"),
+    new Trivia("Who founded Microsoft?", "Bill Gates"),
+];
+
+
+function saveDataToLocalStorage() {
+    localStorage.setItem('users', JSON.stringify(users));
+    localStorage.setItem('loggedInIndex', JSON.stringify(loggedInIndex));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+try {
+    document.getElementById('name').innerHTML = users[loggedInIndex].name;
+    document.getElementById('email').innerHTML = users[loggedInIndex].email;
+    document.getElementById('username').innerHTML = users[loggedInIndex].username;
+} catch (e) {
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadCartItems();
+});
+
+function loadCartItems() {
+    const cartContainer = document.getElementById('cartItems');
+    if (cartContainer) {
+        cartContainer.innerHTML = '';
+
+        const cart = users[loggedInIndex].cart;
+
+        if (cart.length > 0) {
+            var table = document.createElement('table');
+            table.classList.add('cart-table');
+
+            cart.forEach(function (item, index) {
+                var row = table.insertRow();
+
+                var cell1 = row.insertCell(0);
+                cell1.innerHTML = `<img src="${item.imagePath}" alt="${item.productName}" width="100">`;
+
+                var cell2 = row.insertCell(1);
+                cell2.innerHTML = `
+                    <div class="item-details">
+                        <h3>${item.productName}</h3>
+                        <p>${item.price}</p>
+                        <button class="remove-from-cart">Remove</button>
+                    </div>
+                `;
+
+                var removeButton = cell2.querySelector('.remove-from-cart');
+                removeButton.addEventListener('click', function () {
+                    var itemIndex = index;
+
+                    cart.splice(itemIndex, 1);
+                    saveDataToLocalStorage();
+                    document.getElementById('cartnavbar').innerHTML = 'Cart (' + users[loggedInIndex].cart.length + ')';
+
+                    loadCartItems();
+                });
+            });
+
+            cartContainer.appendChild(table);
+
+            var totalPrice = cart.reduce((total, item) => total + parseFloat(item.price.replace('$', '')), 0);
+
+            if (totalPrice > 0) {
+                var totalText = document.createElement('h4');
+                totalText.textContent = 'Total Price: $' + totalPrice.toFixed(2);
+                cartContainer.appendChild(totalText);
+            }
+
+            var checkoutButton = document.createElement('button');
+            checkoutButton.classList.add('checkout-button');
+            checkoutButton.textContent = 'Checkout now!';
+            checkoutButton.style.fontWeight = 'bold';
+            checkoutButton.addEventListener('click', function () {
+                alert('Checkout complete! Thank you for shopping with us!');
+            });
+
+            cartContainer.appendChild(checkoutButton);
+            cartContainer.appendChild(checkoutButton);
+        } else {
+            cartContainer.innerHTML = '<p>Your cart is empty!</p>';
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 var sampleUserFound = users.some(user => user.username === sampleUser.username);
 
 if (!sampleUserFound) {
@@ -107,5 +245,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function submitRequest() {
     alert("Request submitted successfully!");
+}
+
+function checkAnswer(answer, correctAnswer){
+    if (answer.toLowerCase() === correctAnswer.toLowerCase()) {
+        alert('Correct! Enjoy your discount by using code "SAVEBIG" upon checkout!');
+    } else {
+        alert('Incorrect answer :(, please try again!');
+    }
+    location.reload();
 }
 
